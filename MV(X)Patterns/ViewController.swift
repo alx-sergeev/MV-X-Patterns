2fc778bb471466ff5210a4b40c1fefa1abceff90
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-01-10 10:28:29 +0400

    Подготовил UI и ViewController для дальнейших работ

--- MV(X)Patterns/ViewController.swift
+++ MV(X)Patterns/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    @IBOutlet weak var tapLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showGreetingPressed() {
+    }
 }
-
