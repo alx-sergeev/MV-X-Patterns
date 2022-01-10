e18f576bfc6e3ec8d6bd57566cf2d1a604db4e90
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-01-10 10:17:04 +0400

    Initial Commit

---
+++ MV(X)Patterns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MV(X)Patterns
+//
+//  Created by Сергеев Александр on 10.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
